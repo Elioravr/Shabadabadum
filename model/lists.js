708cb415821533d80a036acf3a66de6502e8cbbe
Elior Avramovitz <elioravr@gmail.com>
2015-03-28 01:44:51 +0300

    Add schemas to Familyst

--- model/lists.js
+++ model/lists.js
@@ -1,5 +1,38 @@
 Lists = new Mongo.Collection("lists");
 
+var Schemas = {};
+
+Schemas.List = new SimpleSchema({
+    name: {
+        type: String,
+        label: "Name",
+        max: 200
+    },
+    createdAt: {
+      type: Date
+    }
+});
+
+Schemas.Item = new SimpleSchema({
+    title: {
+        type: String,
+        label: "Title",
+        max: 200
+    },
+    createdAt: {
+      type: Date
+    },
+    is_done: {
+      type: Boolean
+    },
+    list: {
+      type: Schemas.List
+    }
+});
+
+Lists.attachSchema(Schemas.List);
+Items.attachSchema(Schemas.Item);
+
 Lists.allow({
   insert: function (userId, item) {
     // return userId && item.owner === userId;
