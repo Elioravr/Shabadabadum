778e063870ad5c298ce543bd8e97f9f084ac04b2
Elior Avramovitz <elioravr@gmail.com>
2015-03-28 01:48:50 +0300

    Add username filter

--- client/users/filters/displayName.js
+++ client/users/filters/displayName.js
@@ -4,6 +4,8 @@ angular.module("familyst").filter('displayName', function () {
       return;
     if (user.profile && user.profile.name)
       return user.profile.name;
+    else if (user.profile && user.profile.firstName)
+      return user.profile.firstName + " " + user.profile.lastName
     else if (user.emails)
       return user.emails[0].address;
     else
