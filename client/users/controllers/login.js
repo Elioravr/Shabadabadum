241cdbe08b645967bef4b5e9376d3fc652520977
Elior Avramovitz <elioravr@gmail.com>
2015-03-27 17:14:41 +0300

    Add Login

--- client/users/controllers/login.js
+++ client/users/controllers/login.js
@@ -1,6 +1,23 @@
-angular.module("familyst").controller("LoginCtrl", ['$scope', '$stateParams',
-  function($scope, $stateParams){
+// angular.module("familyst").controller("LoginCtrl", ['$scope', '$stateParams',
+//   function($scope, $stateParams){
 
-    // $scope.listId = $stateParams.listId;
+//     // $scope.listId = $stateParams.listId;
 
+// }]);
+angular.module('familyst').controller('LoginCtrl', ['$scope', '$rootScope', '$state', '$ionicPopup', function($scope, $rootScope, $state, $ionicPopup) {
+  $scope.goToSignup = function() {
+    $state.go('register');
+  };
+
+  $scope.login = function() {
+    Meteor.loginWithPassword($scope.emailAddress.toLowerCase(), $scope.password, function(err) {
+      if (err)
+        $ionicPopup.alert({
+          title : 'Error Occurred',
+          template : 'Error occurred while logging in.'
+        });
+      else
+        $state.go('lists');
+    });
+  };
 }]);
