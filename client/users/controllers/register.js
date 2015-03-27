1c5ca0b8348ce4b0d1784438630c7425b021be37
Elior Avramovitz <elioravr@gmail.com>
2015-03-28 01:48:09 +0300

    Add register view + controller

--- client/users/controllers/register.js
+++ client/users/controllers/register.js
@@ -3,6 +3,26 @@ angular.module('familyst').controller('RegisterCtrl', ['$scope', '$rootScope', '
     $state.go('login');
   };
 
+  $scope.register = function () {
+    if ($scope.password === $scope.passwordConfirmation) {
+      Accounts.createUser({
+        email: $scope.emailAddress,
+        password: $scope.password,
+        profile: {
+            firstName: $scope.firstName,
+            lastName: $scope.lastName
+        }
+      }, function () {
+        $state.go("lists");
+      });
+    } else {
+      $ionicPopup.alert({
+        title : 'Oops, Something went wrong',
+        template : 'Your passwords doesn\'t match.'
+      });
+    }
+  }
+
   // $scope.login = function() {
   //   Meteor.loginWithPassword($scope.emailAddress.toLowerCase(), $scope.password, function(err) {
   //     if (err)
