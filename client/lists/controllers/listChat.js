9fe0e84eea115145dd5a5e8cdd3807c7cb817e65
Elior Avramovitz <elioravr@gmail.com>
2015-04-25 20:31:48 +0300

    Fixing chat subscribing

--- client/lists/controllers/listChat.js
+++ client/lists/controllers/listChat.js
@@ -8,7 +8,8 @@ ListChatCtrl.$inject = [
   '$rootScope',
   '$ionicNavBarDelegate',
   '$stateParams',
-  '$ionicScrollDelegate'
+  '$ionicScrollDelegate',
+  '$ionicLoading'
 ];
 
 function ListChatCtrl ($meteor,
@@ -16,23 +17,30 @@ function ListChatCtrl ($meteor,
                        $rootScope,
                        $ionicNavBarDelegate,
                        $stateParams,
-                       $ionicScrollDelegate) {
-
-  $scope.hideTabs = true;
-  $scope.list = $meteor.object(Lists, $stateParams.listId, false);
-  $scope.newMessage = '';
-
-  $scope.$on('$ionicView.beforeEnter', function () {
-    $ionicNavBarDelegate.showBackButton(true);
-  });
-
-  $scope.$on('$ionicView.afterEnter', function () {
-    $ionicScrollDelegate.scrollBottom(true);
-  });
+                       $ionicScrollDelegate,
+                       $ionicLoading) {
 
   // Functions declaration
   $scope.getMomentedDate = getMomentedDate;
   $scope.insertNewMessage = insertNewMessage;
+
+  $scope.showLoading = showLoading;
+  $scope.stopLoading = stopLoading;
+
+  $scope.showLoading();
+
+  // $rootScope.hideTabs = true;
+  $scope.newMessage = '';
+
+  $meteor.subscribe("list", $stateParams.listId).then(function(subscriptionHandle) {
+    $scope.list = $meteor.object(Lists, $stateParams.listId, false);
+    $ionicScrollDelegate.scrollBottom(true);
+    $scope.stopLoading();
+  });
+
+  $scope.$on('$ionicView.beforeEnter', function () {
+    $ionicNavBarDelegate.showBackButton(true);
+  });
 
   function getMomentedDate (date) {
     return moment(date).format("DD/MM hh:mm");
@@ -40,9 +48,10 @@ function ListChatCtrl ($meteor,
 
   function insertNewMessage () {
     newMessage = {
-      content: $scope.newMessage,
-      profile: $rootScope.currentUser.profile,
-      userId:  $rootScope.currentUser._id
+      content:   $scope.newMessage,
+      profile:   $rootScope.currentUser.profile,
+      userId:    $rootScope.currentUser._id,
+      createdAt: new Date()
     };
 
     $scope.list.messages.push(newMessage);
@@ -55,5 +64,15 @@ function ListChatCtrl ($meteor,
         console.log(arguments);
       }
     );
+  }
+
+  function showLoading () {
+    $ionicLoading.show({
+      templateUrl: "client/users/views/loading.ng.html"
+    });
+  }
+
+  function stopLoading () {
+    $ionicLoading.hide();
   }
 }
