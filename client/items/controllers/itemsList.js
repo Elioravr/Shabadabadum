e7c8a3efa97cef44aff97365c993dae5e2e7148d
Elior Avramovitz <elioravr@gmail.com>
2015-04-28 22:49:50 +0300

    Fix checkbox icon duplicate bug

--- client/items/controllers/itemsList.js
+++ client/items/controllers/itemsList.js
@@ -30,6 +30,8 @@ function ItemsListCtrl ($scope,
   $scope.remove = remove;
   $scope.addItemMessage = addItemMessage;
   $scope.itemsLeft = itemsLeft;
+  $scope.items = items;
+  $scope.getItemCheckboxIcon = getItemCheckboxIcon;
   $scope.showLoading = showLoading;
   $scope.stopLoading = stopLoading;
 
@@ -46,6 +48,7 @@ function ItemsListCtrl ($scope,
   });
 
   function insert () {
+    $scope.newItem.createdAt = new Date();
     $scope.newItem.isDone = false;
     $scope.list.items.push($scope.newItem);
     $scope.list.save().then(
@@ -78,8 +81,15 @@ function ItemsListCtrl ($scope,
   }
 
   function markAsDone (item) {
+    console.log("markAsDone start");
+    $scope.showLoading();
     item.isDone = !item.isDone;
-    $scope.list.save();
+    $scope.list.save().then(
+      function () {
+        console.log("markAsDone ends");
+        $scope.stopLoading();
+      }
+    );
   }
 
   function remove (item) {
@@ -88,11 +98,35 @@ function ItemsListCtrl ($scope,
     $scope.list.save();
   }
 
+  function items () {
+    if (!$scope.list) {
+      return [];
+    }
+
+    var items = $scope.list.items.sort(function (a, b) {
+      return b.createdAt - a.createdAt;
+    });
+    return items;
+  }
+
   function itemsLeft () {
+    if (!$scope.list) {
+      return 0;
+    }
+
     left = _.filter($scope.list.items, function(item) {
       return(item.isDone !== true);
     });
     return left.length;
+  }
+
+  function getItemCheckboxIcon (item) {
+    if (item.isDone === true) {
+      return 'ion-android-checkbox-outline'
+    }
+    else {
+      return 'ion-android-checkbox-outline-blank'
+    }
   }
 
   function showLoading () {
