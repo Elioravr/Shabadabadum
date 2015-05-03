c99c92dc1dfbcfdda2998212bc1a03faa8743807
Elior Avramovitz <elioravr@gmail.com>
2015-05-03 22:44:24 +0300

    Native configurations added
    1. Facebook accessRule.
    2. Native icons.
    3. Native splashes.

---
+++ mobile-config.js
@@ -0,0 +1,36 @@
+App.accessRule("*://graph.facebook.com/*");
+App.accessRule("*://fbcdn-profile-a.akamaihd.net/*");
+App.accessRule("*://engine.kadira.io/simplentp/sync");
+
+App.icons({
+  'iphone': 'public/icon-1.png',
+  'iphone_2x': 'public/icon-1.png',
+  'iphone_3x': 'public/icon-1.png',
+  'ipad': 'public/icon-1.png',
+  'ipad_2x': 'public/icon-1.png',
+  'android_ldpi': 'public/icon-1.png',
+  'android_mdpi': 'public/icon-1.png',
+  'android_hdpi': 'public/icon-1.png',
+  'android_xhdpi': 'public/icon-1.png'
+});
+
+App.launchScreens({
+  'iphone': 'public/splash-2.png',
+  'iphone_2x': 'public/splash-2.png',
+  'iphone5': 'public/splash-2.png',
+  'iphone6': 'public/splash-2.png',
+  'iphone6p_portrait': 'public/iPhone-6-plus-Familyst-splash.png',
+  'iphone6p_landscape': 'public/splash-2.png',
+  'ipad_portrait': 'public/splash-2.png',
+  'ipad_portrait_2x': 'public/splash-2.png',
+  'ipad_landscape': 'public/splash-2.png',
+  'ipad_landscape_2x': 'public/splash-2.png',
+  'android_ldpi_portrait': 'public/splash-2.png',
+  'android_ldpi_landscape': 'public/splash-2.png',
+  'android_mdpi_portrait': 'public/splash-2.png',
+  'android_mdpi_landscape': 'public/splash-2.png',
+  'android_hdpi_portrait': 'public/splash-2.png',
+  'android_hdpi_landscape': 'public/splash-2.png',
+  'android_xhdpi_portrait': 'public/splash-2.png',
+  'android_xhdpi_landscape': 'public/splash-2.png'
+});
